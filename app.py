8e3166c2f69b77e54ecf32483740df6c70842746
Ben Christians <bbc7909@rit.edu>
2019-01-23 13:51:14 -0500

    Added new endpoint and fixed some return values.

--- app.py
+++ app.py
@@ -14,9 +14,11 @@ POSTGRES = {
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
 %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy()
 db.init_app(app)
+session = db.session
 
 
 @app.route('/create_room', methods=['POST'])
@@ -25,8 +27,8 @@ def create_room():
         return "Expected JSON"
 
     newRoom = models.Room(roomName=request.json["roomName"])
-    db.session.add(newRoom)
-    db.session.commit()
+    session.add(newRoom)
+    session.commit()
     return "Created new room"
 
 
@@ -43,8 +45,8 @@ def get_room():
     assignmentsQuery = models.Assignment.query.filter_by(roomId=roomId).all()
     roomJson["assignments"] = list(map(models.Assignment.json, assignmentsQuery))
 
-    # registeredUsersQuery = models.UserRegistration.query.filter_by(roomId=roomId).all()
-    # roomJson["registeredUsers"] = registeredUsersQuery
+    registeredUsersQuery = models.UserRegistration.query.filter_by(roomId=roomId).all()
+    roomJson["registeredUsers"] = list(map(models.UserRegistration.json, registeredUsersQuery))
 
     return jsonify(roomJson)
 
@@ -67,10 +69,42 @@ def create_assignments():
             date=assignment["date"],
             completed=False
         )
-        db.session.add(newAssignment)
+        session.add(newAssignment)
 
-    db.session.commit()
+    session.commit()
     return "Created %s assignment(s)" % len(assignments)
+
+
+@app.route('/delete_assignment', methods=['POST'])
+def delete_assignment():
+    if not request.is_json:
+        return "Expected JSON"
+
+    assignmentId = request.json["assignmentId"]
+
+    assignment = models.Assignment.query.filter_by(assignmentId=assignmentId).first()
+
+    o_session = session.object_session(assignment)
+    o_session.delete(assignment)
+    o_session.commit()
+
+    if( assignment is not None ):
+        return "Assignment with id %s deleted" % assignmentId
+    return "No assignment found with id " + assignmentId
+
+
+@app.route('/register_user', methods=['POST'])
+def register_user():
+    if not request.is_json:
+        return "Expected JSON"
+
+    userId = request.json["userId"]
+    roomId = request.json["roomId"]
+
+    session.add(models.UserRegistration(userId=userId, roomId=roomId))
+    session.commit()
+
+    return "User registered"
 
 
 if __name__ == '__main__':
