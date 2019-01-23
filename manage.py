efc2fcb4675d425b8c07edf301122e5d3f4cf5dc
Ben Christians <bbc7909@rit.edu>
2019-01-23 15:26:02 -0500

    oopsie

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from flask_migrate import MigrateCommand
 import app
 
 manager = Manager(app)
-manager.add_command('db', MigrateCommand)
+manager.add_command('db', MigrateCommand())
 
 if __name__ == '__main__':
     manager.run()
